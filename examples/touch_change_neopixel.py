098de48cb92d62a33ae96d929b03374db9a2496c
Seth Kerr <sethkerr@Seths-Mac-mini.local>
2024-03-22 13:43:11 -0600

    update with linting and gh publish artifact

--- examples/touch_change_neopixel.py
+++ examples/touch_change_neopixel.py
@@ -1,14 +1,23 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2024 Seth Kerr for Oak Development Technologies
+#
+# SPDX-License-Identifier: Unlicense
+
+"""
+Example showing how to use the AT42QT1070 touch sensor!
+"""
+
 import board
 import busio
-import time
+import time  # pylint: disable=C0411
 import odt_at42qt1070
-import neopixel
+import neopixel  # pylint: disable=C0411
 
-num_pixels = 1
+num_pixels = 1  # pylint: disable=C0103
 
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, brightness=0.3, auto_write=False)
 
@@ -17,12 +26,11 @@ i2c = busio.I2C(board.SCL, board.SDA)
 qtouch = odt_at42qt1070.AT42QT1070(i2c)
 
 while True:
-  if qtouch.touched():
-    pixels.fill(GREEN)
-    pixels.show()
-    time.sleep(1)
-  else:
-    pixels.fill(RED)
-    pixels.show()
-    time.sleep(1)
-    
+    if qtouch.touched():
+        pixels.fill(GREEN)
+        pixels.show()
+        time.sleep(1)
+    else:
+        pixels.fill(RED)
+        pixels.show()
+        time.sleep(1)
